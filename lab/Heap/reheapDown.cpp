b4a12d6e236f86715d7c10307215cf7103c8f2c1
TRUONG TUAN KIET <125236176+tuankiet0913@users.noreply.github.com>
2023-12-08 20:46:09 +0700

    Update reheapDown.cpp

--- lab/Heap/reheapDown.cpp
+++ lab/Heap/reheapDown.cpp
@@ -1,31 +1,29 @@
 void reheapDown(int maxHeap[], int numberOfElements, int index)
-{   
-    
-    if (index < 0||index*2 + 1 >= numberOfElements) return;
-    if (maxHeap[index] < maxHeap[(index*2) + 1] && maxHeap[index] < maxHeap[(index*2) + 2]){
-        if (maxHeap[(index * 2) + 1] > maxHeap[(index * 2) + 2]){
-            swap(maxHeap[index], maxHeap[(index*2) + 1]);
-            reheapDown(maxHeap, numberOfElements, (index*2) + 1);
+{
+    int lChild = index*2 + 1;
+    int rChild = index*2 + 2;
+    if (index < 0 || lChild > numberOfElements) return;
+    if (maxHeap[index] < maxHeap[lChild] && maxHeap[index] < maxHeap[rChild]){
+        if (lChild > rChild) {
+            swap(maxHeap[lChild], maxHeap[index]);
+            reheapDown(maxHeap, numberOfElements, lChild);
         } else {
-            swap(maxHeap[index], maxHeap[(index*2) + 2]);
-            reheapDown(maxHeap, numberOfElements, (index*2) + 2);
+            swap(maxHeap[rChild], maxHeap[index]);
+            reheapDown(maxHeap, numberOfElements, rChild);
         }
-    } else if (maxHeap[index] < maxHeap[(index*2) + 1]){
-        swap(maxHeap[index], maxHeap[(index*2) + 1]);
-        reheapDown(maxHeap, numberOfElements, (index*2) + 1);
-    } else if (maxHeap[index] < maxHeap[(index*2) + 2]){
-        swap(maxHeap[index], maxHeap[(index*2) + 2]);
-        reheapDown(maxHeap, numberOfElements, (index*2) + 2);
+    } else if (maxHeap[index] < maxHeap[lChild]){
+        swap(maxHeap[lChild], maxHeap[index]);
+        reheapDown(maxHeap, numberOfElements, lChild);
+    } else if (maxHeap[index] < maxHeap[rChild]){
+        swap(maxHeap[rChild], maxHeap[index]);
+        reheapDown(maxHeap, numberOfElements, rChild);
     }
-    
 }
 
 void reheapUp(int maxHeap[], int numberOfElements, int index)
 {
-    if ((index - 1)/2  < 0) return;
-    
-    while (maxHeap[index] > maxHeap[(index - 1)/2]){
-        swap(maxHeap[index],maxHeap[(index - 1)/2]);
-        index = (index - 1)/2; 
+    while ((index - 1)/2 >= 0 && maxHeap[index] > maxHeap[(index - 1)/2]){
+        swap(maxHeap[index], maxHeap[(index - 1)/2]);
+        index = (index - 1)/2;
     }
 }
